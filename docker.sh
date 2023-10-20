cd33ce338f6cecd7c932770331dc5e70044994a0
BrunoSantos88 <brunosantos88>
2023-10-20 17:59:23 -0300

    cloud

--- docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo curl https://releases.rancher.com/install-docker/19.03.sh | sh 
-sudo usermod -aG docker root
-sudo apt-get install git -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
